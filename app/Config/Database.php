b6fd42cf1675958eb693234b7626c5a3b3ca31a9
Jape2002 <manalopv@gmail.com>
2023-09-26 16:36:42 +0800

    update

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'musicPlaylist',
+        'database'     => 'music_playlist',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
